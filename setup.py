2470869ab797647982445899c6d86ae2ddfb5344
David Baker <dave@matrix.org>
2019-08-01 15:44:38 +0100

    use extra deps: lint

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     description="Black-box integration testing for Matrix Identity Servers",
     long_description=open("README.md").read(),
     install_requires=["Twisted>=19.2.1", "requests>=2.22.0"],
-    extras_require={"test": ["flake8>=3.7.8", "isort>=4.3.21", "black>=19.3b0"]},
+    extras_require={"lint": ["flake8>=3.7.8", "isort>=4.3.21", "black>=19.3b0"]},
 )
