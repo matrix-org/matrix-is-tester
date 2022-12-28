2595343ba6018dfa55f8146be495c3f7e0316b35
Patrick Cloke <patrickc@matrix.org>
2022-12-28 07:39:52 -0500

    Bump black.

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     description="Black-box integration testing for Matrix Identity Servers",
     long_description=open("README.md").read(),
     install_requires=["Twisted>=19.2.1", "requests>=2.22.0", "six>=1.13.0"],
-    extras_require={"lint": ["flake8>=3.7.8", "isort>=4.3.21", "black>=19.3b0"]},
+    extras_require={"lint": ["flake8>=3.7.8", "isort>=4.3.21", "black>=21.6b0"]},
     include_package_data=True,
 )
